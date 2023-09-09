5b2c6e69c24ce558429ff6ff97af5b4d9bd8c855
Luiz Fernando <fernandozlf4@gmail.com>
2023-09-09 15:26:00 -0300

    Alterando

---
+++ src/app/components/botao-menu/botao-menu.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-botao-menu',
+  templateUrl: './botao-menu.component.html',
+  styleUrls: ['./botao-menu.component.scss']
+})
+export class BotaoMenuComponent implements OnInit {
+
+  @Input()
+  descricao = '';
+
+  @Input()
+  selecionado = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+}
--- src/app/components/painel-esquerdo/painel-esquerdo.component.ts
+++ src/app/components/painel-esquerdo/painel-esquerdo.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { faHome, faSearch, faGuitar, faMusic } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-painel-esquerdo',
   templateUrl: './painel-esquerdo.component.html',
   styleUrls: ['./painel-esquerdo.component.scss']
 })
-export class PainelEsquerdoComponent {
+export class PainelEsquerdoComponent implements OnInit {
 
+  // Icones
+  homeIcon = faHome;
+  pesquisarIcon = faSearch;
+  artistasIcon = faGuitar;
+  playlist = faMusic;
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
 }
